5a2fee6578b56b43078f92a3fdd44a0234530717
Dylan J. Gerrits <dyrits@outlook.com>
2023-07-31 13:03:46 +0200

    🍱 New data file: products.js

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const typeDefs = `
-  type Query {
-    hello: String
-    }
+import products from "./products.js";
+
+const typeDefs = `#graphql
+type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    image: String!
+    onSale: Boolean!
+}
+type Query {
+    products: [Product!]!
+    product(id: ID!): Product
+}
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello world!"
+    products: () => products,
+    product: (_, { id }) => {
+      return products.find((product) => product.id === id);
+    }
   }
 };
 
